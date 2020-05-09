e6874a77dfd46b1af49d1aa5c3bd5997eab26058
wahyuptama <wahyulucky28@gmail.com>
2020-05-09 13:21:53 +0700

    menambahkan template untuk sistem

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,8 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(ProvinceTableSeeder::class);
-        // $this->call(CityTableSeeder::class);
+        $this->call(ProvinceTableSeeder::class);
+        $this->call(CityTableSeeder::class);
         $this->call(DistrictTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
     }
 }
